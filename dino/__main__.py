3e466a30d462758883890c3118e7da4b6348a120
Eric Hennenfent <ehennenfent@squareup.com>
2022-08-24 01:32:08 -0500

    Refactor driver

--- dino/__main__.py
+++ dino/__main__.py
@@ -43,76 +43,30 @@ def main():
     # Add a buffer to store the last several samples
     buffer = Buffer()
 
+    # Add a simple state machine to tare the data and calculate user weight/velocity/position
     physics = PhysicsSolver(buffer)
 
     # Create some patterns
-    force_matcher.register_pattern(
-        "steady_1s",
-        (eq_1p,) * SAMPLES_PER_SEC,  # 20 samples within 1% of each other
-        physics.calibrate_steady_state,
+    register_default_patterns(force_matcher, physics, state_machine, velocity_matcher)
+
+    # Set up something to do when we detect a jump or a duck
+    setup_jump_duck_handlers(
+        state_machine,
+        lambda color: plotter.draw_vertical_line(buffer.last_item[0], color),
     )
 
-    velocity_matcher.register_pattern(
-        "steady_velocity",
-        (mag_rel(operator.lt, STABLE_THRESH),)
-        * (SAMPLES_PER_SEC // 5),  # 20 samples within 1% of each other
-        partial(state_machine.receive_event, Event.VELOCITY_STEADY),
-    )
-
-    velocity_matcher.register_pattern(
-        "negative",
-        (abs_rel(operator.lt, NEG_THRESH),)
-        * SHORT_SAMPLES,  # 20 samples within 1% of each other
-        partial(state_machine.receive_event, Event.VELOCITY_NEGATIVE),
-    )
-
-    velocity_matcher.register_pattern(
-        "positive_small",
-        (abs_rel(operator.gt, POS_SMALL_THRESH),)
-        * SHORT_SAMPLES,  # 20 samples within 1% of each other
-        partial(state_machine.receive_event, Event.VELOCITY_POSITIVE_SMALL),
-    )
-
-    velocity_matcher.register_pattern(
-        "positive_large",
-        (abs_rel(operator.gt, POS_LARGE_THRESH),)
-        * SHORT_SAMPLES,  # 20 samples within 1% of each other
-        partial(state_machine.receive_event, Event.VELOCITY_POSITIVE_LARGE),
-    )
-
-    def draw_vline(color):
-        current_ts, _ = buffer.last_item
-        plotter.draw_vertical_line(current_ts, color)
-
-    state_machine.register_callback(
-        State.STEADY, State.JUMPING, partial(draw_vline, "green")
-    )
-    state_machine.register_callback(
-        State.DUCKING, State.JUMPING, partial(draw_vline, "green")
-    )
-    state_machine.register_callback(
-        State.STEADY, State.DUCKING, partial(draw_vline, "red")
-    )
-
+    # Plot the data as it comes in
     def pass_tared_to_plotter(last_item):
         ts, y = last_item
         plotter.get_differentiable_series("Force").append(
             (ts, physics.correct_for_tare(y))
         )
 
-    # Plot the data as it comes in
     buffer.register_callback(
         partial(
             Buffer.call_with_last_item,
             pass_tared_to_plotter,
         )
-    )
-
-    # Attempt to pattern match on incoming data
-    buffer.register_callback(partial(Buffer.call_with_underlying, force_matcher.match))
-
-    physics.velocity.register_callback(
-        partial(Buffer.call_with_underlying, velocity_matcher.match)
     )
 
     physics.velocity.register_callback(
@@ -129,6 +83,14 @@ def main():
     #     )
     # )
 
+    # Attempt to pattern match on incoming data
+    buffer.register_callback(partial(Buffer.call_with_underlying, force_matcher.match))
+
+    physics.velocity.register_callback(
+        partial(Buffer.call_with_underlying, velocity_matcher.match)
+    )
+
+    # Either ingest or simulate the data
     if args.command == "plot":
         # Create a reader that feeds data to the buffer
         reader = OpenScaleReader(buffer.append)
@@ -146,6 +108,7 @@ def main():
         )
 
     elif args.command == "simulate":
+        # Read a trace of weights from a file
         reader = Simulator(open(args.simulation_data_file, "r"), buffer.append)
 
         runner = Thread(
@@ -163,6 +126,46 @@ def main():
 
     # Kill the thread reading the data
     reader.stop()
+
+
+def register_default_patterns(force_matcher, physics, state_machine, velocity_matcher):
+    force_matcher.register_pattern(
+        "steady_1s",
+        (eq_1p,) * SAMPLES_PER_SEC,  # 20 samples within 1% of each other
+        physics.calibrate_steady_state,
+    )
+    velocity_matcher.register_pattern(
+        "steady_velocity",
+        (mag_rel(operator.lt, STABLE_THRESH),) * (SAMPLES_PER_SEC // 5),
+        partial(state_machine.receive_event, Event.VELOCITY_STEADY),
+    )
+    velocity_matcher.register_pattern(
+        "negative",
+        (abs_rel(operator.lt, NEG_THRESH),) * SHORT_SAMPLES,
+        partial(state_machine.receive_event, Event.VELOCITY_NEGATIVE),
+    )
+    velocity_matcher.register_pattern(
+        "positive_small",
+        (abs_rel(operator.gt, POS_SMALL_THRESH),) * SHORT_SAMPLES,
+        partial(state_machine.receive_event, Event.VELOCITY_POSITIVE_SMALL),
+    )
+    velocity_matcher.register_pattern(
+        "positive_large",
+        (abs_rel(operator.gt, POS_LARGE_THRESH),) * SHORT_SAMPLES,
+        partial(state_machine.receive_event, Event.VELOCITY_POSITIVE_LARGE),
+    )
+
+
+def setup_jump_duck_handlers(state_machine, draw_vline):
+    state_machine.register_callback(
+        State.STEADY, State.JUMPING, partial(draw_vline, "green")
+    )
+    state_machine.register_callback(
+        State.DUCKING, State.JUMPING, partial(draw_vline, "green")
+    )
+    state_machine.register_callback(
+        State.STEADY, State.DUCKING, partial(draw_vline, "red")
+    )
 
 
 if __name__ == "__main__":
