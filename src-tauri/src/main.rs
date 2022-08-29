16faff66d47bf9038daf0c2d7a266eb36c06fdfb
Eric Hennenfent <ecapstone@gmail.com>
2022-08-29 01:26:18 -0700

    Read events from a UDP socket

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,10 @@
     windows_subsystem = "windows"
 )]
 
+use std::net::UdpSocket;
 use tauri::Manager;
+
+const HOSTPORT: &str = "127.0.0.1:12345";
 
 fn main() {
     tauri::Builder::default()
@@ -11,14 +14,29 @@ fn main() {
             jump(app.get_window("main").unwrap());
             Ok(())
         })
-        .on_page_load(|app, _ev| {})
+        .on_page_load(|_app, _ev| {})
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 fn jump(window: tauri::Window) {
+    let socket = UdpSocket::bind(HOSTPORT).expect("Could not bind to socket");
+    socket
+        .set_read_timeout(None)
+        .expect("set_read_timeout call failed");
+
+    let mut buf = [0; 2];
+
     std::thread::spawn(move || loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        window.emit("jump", {}).unwrap();
+        match socket.recv_from(&mut buf) {
+            Ok((_amt, _src)) => {
+                let parsed: &str = std::str::from_utf8(&buf).unwrap().trim();
+                println!("Received: {}", parsed);
+                window.emit(parsed, {}).unwrap();
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
     });
 }
